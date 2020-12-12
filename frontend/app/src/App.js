7a85897bcd7cc4675298e8675883bb04e346c477
BBrownley <brendobrownley@yahoo.com>
2020-12-11 21:11:05 -0500

    Added display of blogs from local DB

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
+import PostList from "./components/PostList";
+
+import postService from "./services/posts";
+
+const Wrapper = styled.div`
+  max-width: 1260px;
+  margin: auto;
+  background-color: white;
+  min-height: 100vh;
+  padding: 10px;
+`;
+
+const Body = styled.div`
+  background-color: #eff0f2;
+  color: #333;
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  min-height: 100vh;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 400;
+`;
+
+const Branding = styled.h1`
+  margin: 0;
+  padding: 10px 0;
+`;
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    const posts = await postService.getAll();
+    setPosts(posts);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <div className="App">
+        <Wrapper>
+          <Branding>Hello! ^_^</Branding>
+          <PostList posts={posts} />
+        </Wrapper>
+      </div>
+    </Body>
   );
-}
+};
 
 export default App;
