a038a87f6ebe80c1ddd2307ec77609387bc0c275
BBrownley <brendobrownley@yahoo.com>
2020-12-27 04:08:25 -0500

    Added tables to DB along with dummy data for groups, posts, and users

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  password: "",
   user: "root",
-  database: "mydb"
+  database: "reddit-clone"
 });
 
-connection.connect(e => {
-  if (e) {
+connection.connect(err => {
+  if (err) {
     console.error(`error connecting: ${err.stack}`);
   }
   console.log(`connected as id ${connection.threadId}`);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3306; // Make it use .env variable later
+const PORT = 5000; // Make it use .env variable later
 
 const posts = require("./db/posts");
 
