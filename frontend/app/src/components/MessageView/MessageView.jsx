cb7cc4826e997b5ef7543a970e5f3ab78b1db5b7
BBrownley <brendobrownley@yahoo.com>
2021-03-02 01:07:32 -0500

    Added reply to messages

--- frontend/app/src/components/MessageView/MessageView.jsx
+++ frontend/app/src/components/MessageView/MessageView.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Link, useLocation, useHistory } from "react-router-dom";
+
+import messageService from "../../services/messages";
 
 import {
   Message,
@@ -10,8 +13,24 @@ import {
 } from "./MessageView.elements";
 
 export default function MessageView() {
+  const [reply, setReply] = useState("");
+  const [replyOpen, setReplyOpen] = useState(false);
+
   const location = useLocation();
   const history = useHistory();
+  const currentUser = useSelector(state => state.user);
+
+  const sendReply = () => {
+    const message = {
+      sender_id: currentUser.userId,
+      recipient_id: location.state.sender,
+      content: reply,
+      has_read: 0,
+      subject: `Re: ${location.state.subject}`
+    };
+    messageService.send(message);
+    history.push("/inbox");
+  };
 
   return (
     <Message>
@@ -23,8 +42,22 @@ export default function MessageView() {
       <MessageBody>{location.state.body}</MessageBody>
       <Actions>
         <Link to="/inbox">Back</Link>
-        {location.state.sender ? <Link>Reply</Link> : ""}
+        {location.state.sender ? (
+          <a onClick={() => setReplyOpen(true)}>Reply</a>
+        ) : (
+          ""
+        )}
       </Actions>
+      {replyOpen && (
+        <>
+          <input
+            type="text"
+            value={reply}
+            onChange={e => setReply(e.target.value)}
+          />
+          <button onClick={sendReply}>Send</button>
+        </>
+      )}
     </Message>
   );
 }
