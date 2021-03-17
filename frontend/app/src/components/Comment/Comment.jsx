5d0939cb52b428f2c68ddb23f87626489bfb5914
BBrownley <brendobrownley@yahoo.com>
2021-03-17 03:40:37 -0400

    Updated reply to comment form: form properly resets upon submission

--- frontend/app/src/components/Comment/Comment.jsx
+++ frontend/app/src/components/Comment/Comment.jsx
@@ -118,6 +118,7 @@ export default function Comment(props) {
                     newComment
                   );
                   setReplying(false);
+                  setNewComment("");
                 }}
               >
                 Send
