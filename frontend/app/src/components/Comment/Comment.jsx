4aef836bb23c3253d7f97ae3a7dea1c9ac4ef521
BBrownley <brendobrownley@yahoo.com>
2021-03-17 23:31:26 -0400

    Implemented comment voting

--- frontend/app/src/components/Comment/Comment.jsx
+++ frontend/app/src/components/Comment/Comment.jsx
@@ -40,7 +40,10 @@ export default function Comment(props) {
   const [replying, setReplying] = useState(false);
   const [children, setChildren] = useState([]);
   const [newComment, setNewComment] = useState("");
+  const [initialVoteValue, setInitialVoteValue] = useState(0);
   const [commentScoreDelta, setCommentScoreDelta] = useState(0);
+
+  
 
   const history = useHistory();
   const match = useRouteMatch("/groups/:groupName/:groupId");
@@ -55,6 +58,10 @@ export default function Comment(props) {
       setChildren(children);
     };
     fetchChildren();
+
+    if (existingCommentVote) {
+      setInitialVoteValue(existingCommentVote.vote_value);
+    }
   }, []);
 
   const sendNotificationToRepliedUser = (repliedUser, newComment) => {
@@ -77,7 +84,11 @@ export default function Comment(props) {
       ) {
         console.log("removing vote");
         dispatch(removeVote(currentCommentId));
-        setCommentScoreDelta(existingCommentVote.vote_value === 1 ? -1 : 1);
+        setCommentScoreDelta(
+          existingCommentVote.vote_value === 1
+            ? commentScoreDelta - 1
+            : commentScoreDelta + 1
+        );
       } else {
         console.log("change vote");
         const newVoteValue = action === "upvote" ? 1 : -1;
@@ -85,17 +96,50 @@ export default function Comment(props) {
         console.log(newVoteValue);
 
         dispatch(changeVote(currentCommentId, newVoteValue));
-        setCommentScoreDelta(action === "upvote" ? 1 : -1);
+
+        const newScoreDelta = () => {
+          if (initialVoteValue === 0) {
+            return action === "upvote" ? 1 : -1;
+          } else if (initialVoteValue === 1) {
+            return action === "upvote" ? 0 : -2;
+          } else {
+            return action === "upvote" ? 2 : 0;
+          }
+        };
+
+        setCommentScoreDelta(newScoreDelta());
       }
     } else {
       if (action === "upvote") {
+        const newScoreDelta = () => {
+          if (initialVoteValue === 0) {
+            return 1;
+          } else if (initialVoteValue === 1) {
+            return 0;
+          } else {
+            // -1
+            return 2;
+          }
+        };
+
         console.log("upvote");
         dispatch(vote(currentCommentId, 1));
-        setCommentScoreDelta(1);
+        setCommentScoreDelta(newScoreDelta);
       } else {
+        const newScoreDelta = () => {
+          if (initialVoteValue === 0) {
+            return -1;
+          } else if (initialVoteValue === 1) {
+            return -2;
+          } else {
+            // -1
+            return 0;
+          }
+        };
+
         console.log("downvote");
         dispatch(vote(currentCommentId, -1));
-        setCommentScoreDelta(-1);
+        setCommentScoreDelta(newScoreDelta());
       }
     }
   };
@@ -109,6 +153,7 @@ export default function Comment(props) {
           height="35"
         ></img>
       </div>
+      <p>{initialVoteValue}</p>
       <MainContent>
         <Link to={`/users/${props.comment.user_id}`}>
           {props.comment.username}
@@ -121,9 +166,7 @@ export default function Comment(props) {
               onClick={() => handleVoteComment("upvote")}
               upvoted={existingCommentVote?.vote_value === 1 ? true : false}
             />
-            <span>
-              {_.clamp(props.comment.comment_score + commentScoreDelta, 0, 99)}
-            </span>
+            <span>{props.comment.comment_score + commentScoreDelta}</span>
             <CommentVoteButton
               name="thumbs-down"
               onClick={() => handleVoteComment("downvote")}
